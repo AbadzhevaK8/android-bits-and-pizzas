e6fcdb69d27a7357179b3c33b857a3304a923648
k8abadzheva <make.me.loco@gmail.com>
2024-11-23 20:12:54 +0300

    Refactor: Use View Binding in OrderFragment
    Refactor the OrderFragment to use View Binding. This replaces the previous findViewById calls with generated binding classes, improving performance and type safety.
    
    Additionally, remove unused boilerplate code and companion object from the OrderFragment.

--- app/src/main/java/com/abadzheva/bitsandpizzas/MainActivity.kt
+++ app/src/main/java/com/abadzheva/bitsandpizzas/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.abadzheva.bitsandpizzas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::bind, R.id.main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
