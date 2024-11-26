66097fce20a9b33650bbe8c2d1d6bf0a339e1755
k8abadzheva <make.me.loco@gmail.com>
2024-11-26 22:39:40 +0300

    feat: Implement pizza order functionality in OrderFragment
    Implement pizza ordering functionality in the OrderFragment.
    
    Users can now select a pizza type (Diavolo or Funghi) and choose optional extras (Parmesan and Chili oil).
    The order is sent by clicking a floating action button, which displays the order summary in a Snackbar.
    
    Additionally, the fragment now includes a toolbar for better navigation and visual appeal.

--- app/src/main/java/com/abadzheva/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/abadzheva/bitsandpizzas/OrderFragment.kt
@@ -2,10 +2,13 @@ package com.abadzheva.bitsandpizzas
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.abadzheva.bitsandpizzas.databinding.FragmentOrderBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment(R.layout.fragment_order) {
     private val binding by viewBinding(FragmentOrderBinding::bind)
@@ -17,5 +20,24 @@ class OrderFragment : Fragment(R.layout.fragment_order) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
+        binding.fab.setOnClickListener {
+            val pizzaType = binding.pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val textNoChoice = "You need to choose a pizza type"
+                Toast.makeText(activity, textNoChoice, Toast.LENGTH_SHORT).show()
+            } else {
+                var textChoice = (
+                    when (pizzaType) {
+                        R.id.radio_diavolo -> "Diavolo pizza"
+                        else -> "Funghi pizza"
+                    }
+                )
+                val parmesan = view.findViewById<Chip>(R.id.parmesan)
+                textChoice += if (parmesan.isChecked) ", extra parmesan" else ""
+                val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
+                textChoice += if (chiliOil.isChecked) ", extra chili oil" else ""
+                Snackbar.make(binding.fab, textChoice, Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
