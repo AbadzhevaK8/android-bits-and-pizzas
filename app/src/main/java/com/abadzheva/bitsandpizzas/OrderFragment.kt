db7068cd4f5e080836e6e85974cb033d1fe9feeb
k8abadzheva <make.me.loco@gmail.com>
2024-12-04 23:01:48 +0300

    Refactor: Use View Binding in OrderFragment
    Refactor the OrderFragment to use View Binding. This replaces the previous findViewById calls with generated binding classes, improving performance and type safety.
    
    Additionally, remove unused boilerplate code and companion object from the OrderFragment.

--- app/src/main/java/com/abadzheva/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/abadzheva/bitsandpizzas/OrderFragment.kt
@@ -1,23 +1,26 @@
 package com.abadzheva.bitsandpizzas
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import by.kirich1409.viewbindingdelegate.viewBinding
 import com.abadzheva.bitsandpizzas.databinding.FragmentOrderBinding
-import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
-class OrderFragment : Fragment(R.layout.fragment_order) {
-    private val binding by viewBinding(FragmentOrderBinding::bind)
+class OrderFragment : Fragment() {
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onViewCreated(
-        view: View,
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ) {
-        super.onViewCreated(view, savedInstanceState)
+    ): View {
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
         binding.fab.setOnClickListener {
@@ -32,12 +35,16 @@ class OrderFragment : Fragment(R.layout.fragment_order) {
                         else -> "Funghi pizza"
                     }
                 )
-                val parmesan = view.findViewById<Chip>(R.id.parmesan)
-                textChoice += if (parmesan.isChecked) ", extra parmesan" else ""
-                val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
-                textChoice += if (chiliOil.isChecked) ", extra chili oil" else ""
+                textChoice += if (binding.parmesan.isChecked) ", extra parmesan" else ""
+                textChoice += if (binding.chiliOil.isChecked) ", extra chili oil" else ""
                 Snackbar.make(binding.fab, textChoice, Snackbar.LENGTH_SHORT).show()
             }
         }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
